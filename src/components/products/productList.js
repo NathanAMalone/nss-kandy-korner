57ec48e6336186d8d09f423e2db9eabdee346312
Nathan Malone <nathanamalone@gmail.com>
2022-08-30 16:07:07 -0500

    Kandy Korner finished through Chapter 8

--- src/components/products/productList.js
+++ src/components/products/productList.js
@@ -10,6 +10,7 @@ export const ProductList = () => {
 
     const localKandyUser = localStorage.getItem("kandy_user")
     const kandyUserObject = JSON.parse(localKandyUser)
+    const navigate = useNavigate()
 
     useEffect(
         () => {
@@ -17,13 +18,16 @@ export const ProductList = () => {
                 const topPricedProducts = products.filter(product => product.productPrice > 2)
                 setFiltered(topPricedProducts)
             }
+            else{
+                setFiltered(products)
+            }
         },
         [topPriced]
     )
     
     useEffect(
         () => {
-            fetch("http://localhost:8088/products")
+            fetch("http://localhost:8088/products?_expand=productType&_sort=productName")
                 .then(response => response.json())
                 .then((productArray) => {
                     setProducts(productArray)
@@ -34,30 +38,36 @@ export const ProductList = () => {
 
     useEffect(
         () => {
-            if (kandyUserObject.staff) {
                 setFiltered(products)
-            }
         },
         [products]
     )
 
     return <>
         {
+            <>
+            <button   onClick={() => { setTopPriced(true) } } >Top Priced</button>
+            <button   onClick={() => { setTopPriced(false) } } >All Prices</button>
+            </>
+        }
+        {
             kandyUserObject.staff
             ?<>
-            <button   onClick={() => { setTopPriced(true) } } >Top Priced</button>
+            <button   onClick={() => navigate("/product/create") } >Create Product</button>
+
             
-            <h2>List of Products</h2>
-        </>
-            :<h2>Access Denied: Employees Only</h2>
+            </>
+            :""
         }
+        <h2>List of Products</h2>
             <article className="products">
             {
                 filteredProducts.map(
                     (product) => {
                         return <section className="product" key={`product--${product.id}`}>
-                            <header>Name: {product.productName}</header>
-                            <footer>Price: {product.productPrice}</footer>
+                            <div>Name: {product.productName}</div>
+                            <div>Price: {product.productPrice}</div>
+                            <div>Category: {product.productType.candyCategory}</div>
                         </section>
                     }
                     )
